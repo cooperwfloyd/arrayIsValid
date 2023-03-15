3a86fc4804da5b5f92c9f9c14b31ee028e934d78
Cooper Floyd <cooperwfloyd@gmail.com>
2023-03-15 17:47:27 -0500

    simplify with nullish coalescing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const arrayHasLength = require('array-has-length');
 
 const arrayIsValid = (array) => {
-  return !!(
-    arrayHasLength(array) && arrayHasLength(array.filter((item) => !!item))
-  );
+  return !!arrayHasLength?.(array?.filter((item) => !!item));
 };
 
 module.exports = arrayIsValid;
